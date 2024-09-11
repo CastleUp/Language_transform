045e38fa372eda5f795dc9886d8112d6ed3121a7
MaximK <maykca2000@gmail.com>
2024-09-11 19:37:57 +0500

    change variables

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import pandas as pd
 # Инициализируем морфологический анализатор
 morph = pymorphy2.MorphAnalyzer()
 
-# Исходный список животных
-animals = ['Абай', 'Абайский', 'Поселок']
+# Исходный список
+rayons = ['Абай', 'Абайский', 'Поселок']
 
 # Переводим каждое слово в родительный падеж
-animals_genitive = [morph.parse(animal)[0].inflect({'gent'}).word for animal in animals]
+rayons_genitive = [morph.parse(rayon)[0].inflect({'gent'}).word for rayon in rayons]
 
 # Сохраняем результат в DataFrame
-df = pd.DataFrame(animals_genitive, columns=['Родительный падеж'])
+df = pd.DataFrame(rayons_genitive, columns=['Родительный падеж'])
 
 # Выводим DataFrame
 print(df)

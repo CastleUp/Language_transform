a690001c4afcec333c73490f2271e8d16cd24262
karimovdidar <karimovdidar07@gmail.com>
2024-09-17 16:38:38 +0500

    decline1.1

--- request.py
+++ request.py
@@ -1,35 +1,38 @@
 import config_prod
-import psycopg2
 import pandas as pd
+from psycopg2 import connect
 
 # Функция для подключения к базе данных
-def connect_to_db():    
-    conn = psycopg2.connect(
-        dbname=config_prod.DB_NAME, 
-        user=config_prod.DB_USER, 
-        password=config_prod.DB_PASSWORD, 
+def connect_to_db():
+    conn = connect(
+        dbname=config_prod.DB_NAME,
+        user=config_prod.DB_USER,
+        password=config_prod.DB_PASSWORD,
         host=config_prod.DB_HOST,
         port=config_prod.DB_PORT
     )
     return conn
 
-# Функция для выполнения SQL-запроса для получения данных
+# Функция для выполнения SQL-запроса и получения данных
 def fetch_data_from_db(conn):
     query = """
-    SELECT id, name_ru FROM dict_budget_regions 
+    SELECT id, code, name_ru, name_kk FROM dict_budget_regions 
     WHERE beg_date <= '2024-09-01' AND end_date >= '2024-09-01'
-    AND name_ru IS NOT NULL
+    AND name_ru IS NOT NULL AND name_kk IS NOT NULL
     """
     df = pd.read_sql_query(query, conn)
     return df
 
-# Функция для создания новой таблицы в базе данных
+# Функция для создания новой таблицы с результатами
 def create_declined_table(conn):
     query = """
     CREATE TABLE IF NOT EXISTS dict_budget_regions_declined (
         id SERIAL PRIMARY KEY,
+        code VARCHAR(255),
         name_ru VARCHAR(255),
-        name_ru_declined VARCHAR(255)
+        name_ru_declined VARCHAR(255),
+        name_kk VARCHAR(255),
+        name_kk_declined VARCHAR(255)
     );
     """
     with conn.cursor() as cursor:
@@ -40,8 +43,8 @@ def create_declined_table(conn):
 def insert_declined_data(conn, data):
     with conn.cursor() as cursor:
         query = """
-        INSERT INTO dict_budget_regions_declined (id, name_ru, name_ru_declined)
-        VALUES (%s, %s, %s)
+        INSERT INTO dict_budget_regions_declined (id, code, name_ru, name_ru_declined, name_kk, name_kk_declined)
+        VALUES (%s, %s, %s, %s, %s, %s)
         """
         cursor.executemany(query, data)
         conn.commit()
--- test.py
+++ test.py
@@ -2,14 +2,11 @@ import pymorphy2
 import pandas as pd
 from request import connect_to_db, fetch_data_from_db, create_declined_table, insert_declined_data
 
-# Инициализируем морфологический анализатор для русского языка
+# Инициализация морфологического анализатора для русского языка
 morph = pymorphy2.MorphAnalyzer()
 
-# Функция для склонения фраз на русском языке
+# Функция для склонения слов на русском языке
 def decline_russian(phrase):
-    """
-    Склонение фраз на русском языке в родительный падеж с учетом дефисов и маркеров.
-    """
     stop_words = ['село', 'район', 'поселок', 'г.', 'город']
 
     def restore_case(word, original_word):
@@ -23,7 +20,6 @@ def decline_russian(phrase):
         return word.replace('ё', 'е')
 
     def process_word(word):
-        """Склоняем одно слово и восстанавливаем его регистр."""
         parsed_word = morph.parse(word)[0]
         declined_word = parsed_word.inflect({'gent'})
         if declined_word:
@@ -31,7 +27,6 @@ def decline_russian(phrase):
         else:
             return word
 
-   # Обрабатываем фразы с точками
     if '.' in phrase:
         parts = phrase.split('.')
         first_part = parts[0] + '.'
@@ -54,28 +49,72 @@ def decline_russian(phrase):
     
     return ' '.join(declined_words)
 
-# Функция для склонения и записи данных в базу
+# Функция для склонения слов на казахском языке
+def decline_kazakh(phrase):
+    words = phrase.split()
+    
+    # Обрабатываем последнее слово или группу слов с дефисом
+    last_word = words[-1]
+    
+    if '-' in last_word:  # Если есть дефис
+        parts = last_word.split('-')
+        declined_last_part = add_kazakh_genitive(parts[-1])  # Склоняем только последнюю часть
+        declined_last_word_full = '-'.join(parts[:-1] + [declined_last_part])
+    else:
+        declined_last_word_full = add_kazakh_genitive(last_word)
+
+    return ' '.join(words[:-1] + [declined_last_word_full])
+
+# Функция для добавления окончания родительного падежа на казахском
+def add_kazakh_genitive(word):
+    def is_vowel(char):
+        return char in 'аеёиоуыэюяіүұө'
+
+    def is_front_vowel(char):
+        return char in 'еіүө'
+
+    def is_back_vowel(char):
+        return char in 'аоұы'
+
+    last_letter = word[-1]
+    if is_vowel(last_letter):
+        if is_front_vowel(last_letter):
+            return word + 'нің'
+        else:
+            return word + 'ның'
+    else:
+        root_vowels = [char for char in word if is_vowel(char)]
+        if root_vowels:
+            last_vowel = root_vowels[-1]
+            if is_front_vowel(last_vowel):
+                return word + 'тің' if last_letter in 'қкптсшч' else word + 'нің'
+            elif is_back_vowel(last_vowel):
+                return word + 'тың' if last_letter in 'қкптсшч' else word + 'ның'
+        return word
+
+# Основная функция для обработки данных
 def process_districts():
     conn = connect_to_db()
 
     # Получаем данные из базы данных
     df = fetch_data_from_db(conn)
 
-    # Склоняем названия районов
+    # Склоняем названия районов на русском и казахском
     df['name_ru_declined'] = [decline_russian(district) for district in df['name_ru'].tolist()]
-    print(df)
-
+    df['name_kk_declined'] = [decline_kazakh(district) for district in df['name_kk'].tolist()]
+    
     # Подготовка данных для вставки в новую таблицу
-    data = list(df[['id', 'name_ru', 'name_ru_declined']].itertuples(index=False, name=None))
+    data = list(df[['id', 'code', 'name_ru', 'name_ru_declined', 'name_kk', 'name_kk_declined']].itertuples(index=False, name=None))
 
     # Создаем таблицу для склоненных названий
-    create_declined_table(conn)
+    #create_declined_table(conn)
 
     # Вставляем данные в таблицу
-    insert_declined_data(conn, data)
+    #insert_declined_data(conn, data)
 
     conn.close()
     print("Данные успешно сохранены в новую таблицу.")
+    print(df)
 
 if __name__ == "__main__":
     process_districts()
